8907883a4eee619c7bee5f71a963fd0e23ca6963
Richard Staines <richard.staines@livce.com>
2023-11-22 11:33:01 +0000

    Add ex_div_date onto positons grid

--- position/views.py
+++ position/views.py
@@ -1,10 +1,13 @@
+from django.db.models.expressions import RawSQL
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import DateField, CharField, Value, Window, F, Max, ExpressionWrapper, QuerySet
 
+from DividendSchedule.models import DividendSchedule
 from Tools import LoadCsv
 from trade.models import Trade
 from .forms import PositionForm, PositionCsvLoaderForm
@@ -44,7 +47,21 @@ class PositionListView(LoginRequiredMixin, ListView):
     login_url = "/login"
 
     def get_queryset(self):
-        return Position.objects.all()
+        qs = Position.objects.all()
+      #  instruments = [item.instrument.id for item in qs]
+      #  ds_qs = (DividendSchedule.objects
+      #           .filter(instrument__in=instruments)
+      #           )
+      #  ex_div_lookup = {item.instrument.id: item.ex_div_date for item in ds_qs}
+
+        qs = qs.annotate(ex_div_date=RawSQL("select max(ex_div_date) as ex_div_date "
+                                             "from app_dividendschedule "
+                                             "where instrument_id = app_position.instrument_id "
+                                             "GROUP by instrument_id", []),
+                      )
+        return qs
+
+    # + Value(ex_div_lookup.get(551) )
 
 
 class PositionDetailView(DetailView):
@@ -54,10 +71,9 @@ class PositionDetailView(DetailView):
 
 
 def csv_load_form(request):
-
     # Create a form instance and populate it with data from the request (binding):
     if request.method == 'POST':
-        form =  PositionCsvLoaderForm(request.POST, request.FILES)
+        form = PositionCsvLoaderForm(request.POST, request.FILES)
         if form.is_valid():
             # Process the form data (e.g., send an email)
             portfolio = form.cleaned_data['portfolio']
