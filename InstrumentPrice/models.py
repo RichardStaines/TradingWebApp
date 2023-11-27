1d8767d8840cbd21918860592aad06d11c6ac5c5
Richard Staines <richard.staines@livce.com>
2023-11-27 10:27:20 +0000

    add instrument price model and migration

---
+++ InstrumentPrice/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class InstrumentPrice(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    price_source = models.CharField(max_length=20, unique=False)
+    open = models.DecimalField(max_digits=12, decimal_places=4)
+    close = models.DecimalField(max_digits=12, decimal_places=4)
+    high = models.DecimalField(max_digits=12, decimal_places=4)
+    low = models.DecimalField(max_digits=12, decimal_places=4)
+    volume = models.DecimalField(max_digits=12, decimal_places=4)
+    price = models.DecimalField(max_digits=12, decimal_places=4)
+    change = models.DecimalField(max_digits=12, decimal_places=4)
+    change_percent = models.DecimalField(max_digits=12, decimal_places=4)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = "app_instrument_price"
+
+    def __str__(self):
+        return f"{self.code}"
--- TradingWebApp/settings.py
+++ TradingWebApp/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cash',
     'trade',
     'position',
+    'InstrumentPrice',
     'CsvLoader',
 ]
 
