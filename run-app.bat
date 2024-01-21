b549edfe6c48cfe87d17fe9aa9765535d4a61c28
Richard Staines <43862493+RichardStaines@users.noreply.github.com>
2024-01-21 16:50:01 +0000

    start django app and react together

--- run-app.bat
+++ run-app.bat
@@ -1,3 +1,11 @@
 call .\venv\Scripts\activate.bat
-python manage.py runserver
+
+start python manage.py runserver
+
+rundll32 url.dll,FileProtocolHandler "http://127.0.0.1:8000/"
+
+echo %cd%
+start-react.bat
+
+
 pause
